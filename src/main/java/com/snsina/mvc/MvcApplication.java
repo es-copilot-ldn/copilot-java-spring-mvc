3a6bbd47a0f192dc24fe58b09286c0c8a4606927
Siavash Namvar <snsinahub@github.com>
2023-08-15 23:35:18 -0400

    Add

--- src/main/java/com/snsina/mvc/MvcApplication.java
+++ src/main/java/com/snsina/mvc/MvcApplication.java
@@ -2,12 +2,14 @@ package com.snsina.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvcApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(MvcApplication.class, args);
 	}
 
 }
